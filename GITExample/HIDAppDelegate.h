708fd9811394b438bd90f3f28f49219d20aea49e
hugointegrasys <hugo_id2004@hotmail.com>
2014-07-09 11:46:24 +0200

    Initial Commit

---
+++ GITExample/HIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HIDAppDelegate.h
+//  GITExample
+//
+//  Created by Hugo Integrasys on 7/9/14.
+//  Copyright (c) 2014 Integrasys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GITExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GITExample
+//
+//  Created by Hugo Integrasys on 7/9/14.
+//  Copyright (c) 2014 Integrasys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HIDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HIDAppDelegate class]));
+    }
+}
